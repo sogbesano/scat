52c961da859d832c0af530118ceb555843272669
sogbesano <sogbesano@gmail.com>
2016-09-02 15:25:30 +0100

    getTime method updated

--- ClientConnection.scala
+++ ClientConnection.scala
@@ -7,7 +7,9 @@ import java.net.InetSocketAddress
 import java.security._
 import java.io._
 import scala.io._
+import java.util.GregorianCalendar
 import java.util.Calendar
+import java.util.Date
 import com.sun.net.ssl.internal.ssl.Provider
 import scala.util.parsing.json._
 
@@ -24,17 +26,19 @@ class ClientConnection(host: InetAddress, port: Int) {
 
   def sendMsg(server: Socket, user: User, msg: String): Unit = {
     val out = new PrintStream(server.getOutputStream())
-    //out.println(this.getCurrDate() + " " + user.username + ": " + msg)
     out.println(this.toMinifiedJson(user.username, msg))
     out.flush()
   }  
 
   private def toMinifiedJson(user: String, msg: String): String = {
-    s"""{"time":"${this.getCurrDate()}","username":"$user","msg":"$msg"}"""
+    s"""{"time":"${this.getTime()}","username":"$user","msg":"$msg"}"""
   }
 
- //change to getHour() + getMinute() + getSecond()
-  def getCurrDate(): String = "(" + Calendar.getInstance().getTime().toString().substring(11, 19) + ")"
+  def getTime(): String = {
+    val cal = Calendar.getInstance()
+    cal.setTime(new Date())
+    "(" + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND) + ")"
+  }
 
   def close(server: Socket): Unit = server.close()
 }
